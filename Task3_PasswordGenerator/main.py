ef60f3a866ef99c1b46a0de8e799c784ac7ffcbf
CHAMAN KUMAR PATEL <chamanpatel822@gmail.com>
2025-07-20 13:13:10 +0530

    Add Task3: PasswordGenerator main.py

---
+++ Task3_PasswordGenerator/main.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+def generate_password(length):
+    
+    characters = string.ascii_letters + string.digits + string.punctuation
+   
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print("=== Password Generator ===")
+
+while True:
+    try:
+        length = int(input("Enter password: "))
+        if length < 6:
+            print("Enter a length of at least 6 for strong password.")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid number.")
+
+password = generate_password(length)
+print("\nYour generated password is:", password)
